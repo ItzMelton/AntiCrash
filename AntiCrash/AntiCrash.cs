4a1f3b7e213d892541dd38e8296ca927d1b8e84b
ItzMelton <152860857+ItzMelton@users.noreply.github.com>
2024-09-05 07:20:23 +0800

    Update AntiCrash.cs

--- AntiCrash/AntiCrash.cs
+++ AntiCrash/AntiCrash.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Reflection;
-using System.Text.RegularExpressions;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Terraria;
-using Terraria.ID;
-using Terraria.Localization;
 using TerrariaApi.Server;
 using TShockAPI;
 using TShockAPI.Hooks;
@@ -14,7 +12,7 @@ namespace AntiCrash;
 [ApiVersion(2, 1)]
 public class AntiCrash : TerrariaPlugin {
     public override string Name => "AntiCrash";
-    public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+    public override Version Version => new Version(1, 0, 8);
     public override string Author => "Melton";
     public override string Description => "A TShock plugin that attempts to prevent various crash exploits.";
 
@@ -29,12 +27,42 @@ public class AntiCrash : TerrariaPlugin {
     public void OnChat(ServerChatEventArgs args) {
         string message = args.Text;
         bool triggered = false;
-        if (message.Split(" ").Any(substring => substring.Length >= 50)) { // if sent message contains a substring with length greater than 50
+        if (args.Handled) return;
+        if (message.Split(" ").Any(substring => substring.Length >= 50)) {
+            // if sent message contains a substring with length greater than 50
             TShock.Players[args.Who].Kick("Sent a message with excessive substring length", true);
+            triggered = true;
+        }
+        else if (ContainsBadCT(message)) {
+            // if sent message contains a badly formatted ct tag
+            TShock.Players[args.Who].Kick("Badly formatted controls touch tag pattern", true);
             triggered = true;
         }
         if (triggered) {
             args.Handled = true;
         }
-    }    
+    }
+    private static bool ContainsBadCT(string message) {
+        string ctPattern = @"\[ct:(1|7),(\d*)\]";
+        // ct pattern
+
+        if (Regex.IsMatch(message, @"\[ct:(1|7),$")) {
+            return true;
+        }
+
+        MatchCollection matches = new Regex(ctPattern).Matches(message);
+
+        foreach (Match match in matches) {
+            // if the message they sent matches the ct pattern
+            string itemIDstring = match.Groups[2].Value;
+            int itemID;
+            if (int.TryParse(itemIDstring, out itemID)) {
+                // if the item ID is a number
+                if (itemID < 0 || itemID >= 5456) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
