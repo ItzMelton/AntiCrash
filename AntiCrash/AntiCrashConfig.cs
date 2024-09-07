9c02a6cfc7bae5d38cbb81de6229de98b812d448
ItzMelton <152860857+ItzMelton@users.noreply.github.com>
2024-09-07 18:49:23 +0800

    Update AntiCrashConfig.cs

--- AntiCrash/AntiCrashConfig.cs
+++ AntiCrash/AntiCrashConfig.cs
@@ -1,1 +1,22 @@
+using System.IO;
+using TShockAPI;
+using TShockAPI.Configuration;
+/// Config for AntiCrash
 
+namespace AntiCrash
+{
+    public class AntiCrashConfig
+    {
+        public static string FilePath = Path.Combine(TShock.SavePath, "AntiCrash.json");
+        /// FilePath of the AntiCrash config file
+        /// TShock/AntiCrash.json
+        
+        public bool Enabled { get; set; } = true;
+        public int MaxMessageLength { get; set; } = 50;
+        public bool AllowAntiCT { get; set; } = true;
+        /// Settings for AntiCrash.json
+    }
+
+    public class AntiCrashConfigFile : ConfigFile<AntiCrashConfig>
+    { }
+}
