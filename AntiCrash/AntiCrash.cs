df9fc7a69ae2e09970286dacb4f10c3ae635f8a8
ItzMelton <152860857+ItzMelton@users.noreply.github.com>
2025-02-09 19:10:08 +0800

    Update AntiCrash.cs

--- AntiCrash/AntiCrash.cs
+++ AntiCrash/AntiCrash.cs
@@ -13,16 +13,12 @@ namespace AntiCrash;
 public class AntiCrash : TerrariaPlugin
 {
     public override string Name => "AntiCrash";
-    /// The name of the plugin
     
     public override Version Version => new Version(1, 1, 5);
-    /// The version of the plugin
     
     public override string Author => "Melton";
-    /// The author of the plugin
     
     public override string Description => "A TShock plugin that attempts to prevent various crash exploits.";
-    /// The Description of the plugin
 
     private AntiCrashConfig Config;
 
@@ -31,7 +27,7 @@ public class AntiCrash : TerrariaPlugin
 
     public override void Initialize()
     {
-        //Create a new config if one does not exist yet and read them
+        /// Create a new config if there is none
         Config = PluginConfiguration.Load<AntiCrashConfig>();
 
         ServerApi.Hooks.ServerChat.Register(this, OnChat);
@@ -54,7 +50,7 @@ public class AntiCrash : TerrariaPlugin
         base.Dispose(disposing);
     }
 
-    /// called everytime server receives a chat message before being sent to clients
+    /// called everytime server receives a chat message
     public void OnChat(ServerChatEventArgs args)
     {
         if (args.Handled)
@@ -64,38 +60,33 @@ public class AntiCrash : TerrariaPlugin
         bool triggered = false;
 
         if (TShock.Players[args.Who] == null) return;
-        /// If a player doesn't exist or is null then return
-        /// This is to prevent error when a player doesn't exist.
-        
+
+        /// Detecting long messages (Recommended max message 50 or higher, lower will effect its accuracy)
         if (message.Split(" ").Any(substring => substring.Length >= Config.MaxMessageLengthWithoutSpaces))
         {
             TShock.Players[args.Who].Kick("Sent a message with excessive substring length", true);
-            /// If sent a message contains a substring with length greater than 50
-            /// This is the first blocker for blocking long and cted messages
-            /// Kick the player with a kick message in chat (optional: true is on, if you want to not kick an Admin set the true to false)
-            
+    
             triggered = true;
         }
+        
+        /// Detecting symboled messages 
         else if (ContainsBadCT(message))
         {
             if (!Config.AllowAntiCT) 
                 return;
             TShock.Players[args.Who].Kick("Badly formatted controls touch tag pattern", true);
-            /// Else if the message contains a bad character: @"\[ct:(1|7),(\d*)\]
-            /// This is the second blocker for blocking long and cted messages
-            /// Kick the player with a kick message in chat (optional: true is on, if you want to not kick an Admin set the true to false)
-            
+        
             triggered = true;
         }
+
+        /// Detecting short message crashcodes
+        /// projectiles crash ()
         else if (ShortBadCT(message))
         {
             if (!Config.AllowAntiCT) 
                 return;
             TShock.Players[args.Who].Kick("Badly short formatted ct tag pattern", true);
-            /// Else if the message has a short CT: [ct:7,5456]
-            /// This is for blocking short cted messages
-            /// Kick the player with a kick message in chat (optional: if you dont want to kick everyone even Admin set the true to false)
-
+            
             triggered = true;
         }
         
@@ -107,7 +98,7 @@ public class AntiCrash : TerrariaPlugin
     private static bool ContainsBadCT(string message)
     {
         string ctPattern = @"\[ct:(1|7),(\d*)\]";
-        /// The ct pattern
+        /// CT Pattern
 
         if (Regex.IsMatch(message, @"\[ct:(1|7),$"))
         {
@@ -118,7 +109,6 @@ public class AntiCrash : TerrariaPlugin
 
         foreach (Match match in matches)
         {
-            /// if the message they sent matches in the ct pattern
             string itemIDstring = match.Groups[2].Value;
             int itemID;
             if (int.TryParse(itemIDstring, out itemID))
@@ -134,8 +124,6 @@ public class AntiCrash : TerrariaPlugin
     private static bool ShortBadCT(string message)
     {
         return message.Contains("5456");
-        /// Check for exact phrase
-        /// return true if it contains the phrase
     }
 
     public void OnJoin(JoinEventArgs args)
@@ -144,7 +132,6 @@ public class AntiCrash : TerrariaPlugin
         if (player.Name.Contains("5456"))
         {
             player.Kick("Your name contains a bad character! Please change it to something else.", true);
-            /// If the player contains 5456 then kick
         }
     }
 
